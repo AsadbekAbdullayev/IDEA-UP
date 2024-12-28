bf282b8384aee8d7eca56d58a261191b667b05d2
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2024-12-28 20:37:05 +0500

    Refactor signin component to remove unused imports and streamline dependencies

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
-import { FooterComponent } from '../../shared/components/footer/footer.component';
-import { TabComponent } from '../../shared/components/tab/tab.component';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { AccordionListComponent } from '../../shared/components/accordion-list/accordion-list.component';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-signin',
-  imports: [
-    NavbarComponent,
-    TabComponent,
-    FormsModule,
-    MatButtonModule,
-    AccordionListComponent,
-    FooterComponent,
-    NzIconModule,
-    NzInputModule,
-  ],
+  imports: [FormsModule, MatButtonModule, NzIconModule, NzInputModule],
   templateUrl: './signin.component.html',
 })
 export default class SigninComponent {
