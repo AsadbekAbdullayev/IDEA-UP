cd135d9e93c15aa0da99092822c66b1b8824951b
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-01-22 13:37:17 +0500

    Update input.component.ts

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 
-
-
 @Component({
   selector: 'app-generic-input',
   templateUrl: './input.component.html',
