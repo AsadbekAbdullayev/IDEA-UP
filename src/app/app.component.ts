a052e18b00a0ccf15f9f4201c20172f6dbddb05c
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-03-11 08:21:16 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from "@angular/material/button";
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
